da3dde6478ac2989f02bc8fd58acb8850dc72a90
razenry <razenry.code@gmail.com>
2026-02-14 21:34:18 +0700

    Add Aspiration Migration

---
+++ database/migrations/2026_02_14_094918_create_aspirations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aspirations', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('sender_id')->nullable()->constrained('users', 'id');
+            $table->foreignId('staff_id')->nullable()->constrained('users', 'id');
+
+            $table->string('title', 30);
+            $table->string('location', 30);
+
+            $table->text('content');
+            $table->string('photo');
+
+            $table->enum('status', ['accepted', 'process', 'finished', 'rejected']);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aspirations');
+    }
+};
---
+++ database/migrations/2026_02_14_095850_create_feedback_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('sender_id')->nullable()->constrained('users', 'id');
+            $table->foreignId('aspiration_id')->nullable()->constrained('aspirations', 'id');
+
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedback');
+    }
+};
